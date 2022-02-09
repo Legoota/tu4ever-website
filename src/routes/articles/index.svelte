45401ff55719611f08c4361e1a4f7cdd190174f5
Léo Krebs <leokrebs68@gmail.com>
2022-02-09 23:11:30 +0100

    Adding article index page

---
+++ src/routes/articles/index.svelte
@@ -0,0 +1,96 @@
+<script context="module">
+    import DeviceDetector from "svelte-device-detector";
+</script>
+
+<svelte:head>
+	<title>Articles - TU4EVER</title>
+</svelte:head>
+
+<div class="content">
+    <h1>Articles disponibles</h1>
+    <br />
+    <br />
+
+    <DeviceDetector showInDevice="desktop">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <!--
+                    <div class="card card-article" style="width: 20rem;">
+                        <img class="card-img-top" src="images/205-neige.jpg" alt="Article">
+                        <div class="card-body">
+                            <h5 class="card-title">Titre</h5>
+                            <p class="card-text card-article-short">Informations de base</p>
+                            <div class="row mx-auto d-block">
+                                <a sveltekit:prefetch href="/articles/testing-template" class="btn card-article-btn">Lire</a>
+                            </div>
+                        </div>
+                    </div>
+                    -->
+                </div>
+                <div class="col-sm">
+                    <!-- CARTE ICI-->
+                </div>
+                <div class="col-sm">
+                    <!-- CARTE ICI-->
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <!-- CARTE ICI-->
+                </div>
+                <div class="col-sm">
+                    <!-- CARTE ICI-->
+                </div>
+                <div class="col-sm">
+                    <!-- CARTE ICI-->
+                </div>
+            </div>
+        </div>
+    </DeviceDetector>
+
+    <DeviceDetector showInDevice="mobile">
+        <div class="row justify-content-center">
+            <!--
+            <div class="card card-article mobile-card" style="width: 80%;">
+                <img class="card-img-top" src="images/205-neige.jpg" alt="Article">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text card-article-short">Informations de base</p>
+                    <a sveltekit:prefetch href="/articles/testing-template" class="btn card-article-btn">Lire</a>
+                </div>
+            </div>
+            -->
+
+            <!-- CARTE ICI-->
+        </div>
+    </DeviceDetector>
+    
+
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+
+    .card-article {
+        background-color: #787878;
+        margin-bottom: 3rem;
+    }
+
+    .card-article-short {
+        color: #202020;
+    }
+
+    .card-article-btn {
+        background-color: var(--accent-color);
+    }
+
+    .mobile-card {
+        padding-top: .7rem;
+    }
+</style>
