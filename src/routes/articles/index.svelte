41f1d4f4b5235954778887fdf8349dee8f48a3a4
Léo Krebs <leokrebs68@gmail.com>
2022-03-13 10:43:43 +0100

    Adding wheels-repair page

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -16,7 +16,7 @@
             <div class="row">
                 <div class="col-sm">
                     <div class="card card-article" style="width: 20rem;">
-                        <img class="card-img-top" src="images/interior_washing/6.jpg" alt="Article">
+                        <img class="card-img-top" src="images/interior_washing/6.jpg" alt="Intérieur">
                         <div class="card-body">
                             <h5 class="card-title">Nettoyage de l'intérieur</h5>
                             <p class="card-text card-article-short">Nous partageons avec vous la "remise à neuf" de l'intérieur de notre 205. Il y avait du travail...</p>
@@ -27,7 +27,16 @@
                     </div>
                 </div>
                 <div class="col-sm">
-                    <!-- CARTE ICI-->
+                    <div class="card card-article" style="width: 20rem;">
+                        <img class="card-img-top" src="images/wheels_repair/4.jpg" alt="Jantes">
+                        <div class="card-body">
+                            <h5 class="card-title">Réfection des jantes</h5>
+                            <p class="card-text card-article-short">Nous avons restauré des jantes de 205 GTI 1.6 à la maison, et nous vous expliquons les différentes étapes.</p>
+                            <div class="row mx-auto d-block">
+                                <a sveltekit:prefetch href="/articles/wheels-repair" class="btn card-article-btn">Lire</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-sm">
                     <!-- CARTE ICI-->
@@ -56,18 +65,31 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text card-article-short">Informations de base</p>
-                    <a sveltekit:prefetch href="/articles/testing-template" class="btn card-article-btn">Lire</a>
+                    <div class="row mx-auto d-block">
+                        <a sveltekit:prefetch href="/articles/testing-template" class="btn card-article-btn">Lire</a>
+                    </div>
                 </div>
             </div>
             -->
 
-            <div class="card card-article" style="width: 20rem;">
-                <img class="card-img-top" src="images/interior_washing/6.jpg" alt="Article">
+            <div class="card card-article mobile-card" style="width: 80%;">
+                <img class="card-img-top" src="images/interior_washing/6.jpg" alt="Intérieur">
                 <div class="card-body">
                     <h5 class="card-title">Nettoyage de l'intérieur</h5>
                     <p class="card-text card-article-short">Nous partageons avec vous la "remise à neuf" de l'intérieur de notre 205. Il y avait du travail...</p>
                     <div class="row mx-auto d-block">
                         <a sveltekit:prefetch href="/articles/interior-car-washing" class="btn card-article-btn">Lire</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card card-article mobile-card" style="width: 80%;">
+                <img class="card-img-top" src="images/wheels_repair/4.jpg" alt="Jantes">
+                <div class="card-body">
+                    <h5 class="card-title">Réfection des jantes</h5>
+                    <p class="card-text card-article-short">Nous avons restauré des jantes de 205 GTI 1.6 à la maison, et nous vous expliquons les différentes étapes.</p>
+                    <div class="row mx-auto d-block">
+                        <a sveltekit:prefetch href="/articles/wheels-repair" class="btn card-article-btn">Lire</a>
                     </div>
                 </div>
             </div>
