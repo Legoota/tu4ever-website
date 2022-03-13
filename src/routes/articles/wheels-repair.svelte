07794ebd6056f680d29ce39414ee2613de50eea9
Léo Krebs <leokrebs68@gmail.com>
2022-03-13 18:23:07 +0100

    Adding first part of wheels-repair

--- src/routes/articles/wheels-repair.svelte
+++ src/routes/articles/wheels-repair.svelte
@@ -2,146 +2,210 @@
 </script>
 
 <svelte:head>
-	<title>Titre de l'article - TU4EVER</title>
+	<title>Réfection des jantes - TU4EVER</title>
 </svelte:head>
 
 <div class="content">
-	<h1>Titre de l'article</h1>
-
+	<h1>Réfection des jantes 205 GTI 1.6L</h1>
+    <br/>
 	<p class="paragraphe">
-		<span class="accent-color">Paragraphe introductif avec texte couleur</span> - 
-        <span class="secondary-color">Autre couleur pour texte</span> - 
-        <span class="tertiary-color">Autre couleur pour texte</span> -
-        <span class="quartiary-color">Autre couleur pour texte</span> -
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eum ex tempore sunt rem labore sit, et ab in praesentium doloremque, beatae odit assumenda enim! Blanditiis aspernatur quibusdam laboriosam aut.
+		Après avoir acheté notre Peugeot 205, nous avons rapidement émis l'idée de l'équiper de jantes en aluminium.
+        Nous sommes rapidement tombés amoureux du design des jantes de 205 GTI (version  1.6L). 
+        Nous avons alors fait l'aquisition de quatre jantes dans un mauvais état esthétique.
 	</p>
 
-    <!-- IMAGE -->
+    <p class="paragraphe">
+        Nous nous sommes donnés l'objectif de réaliser la réfection de nos jantes directement à la maison,
+        en utilisant du matériel accessible aisément pour les particuliers. Le résultat attendu n'est évidemment pas
+        celui que nous pourrions obtenir en nous rendant chez un professionnel, ou en utilisant des outils dédiés.
+    </p>
 
-	<img src="/images/205-neige.jpg" class="img-fluid rounded mx-auto d-block" alt="Desc">
+    <p class="paragraphe">
+        Voici les différentes étapes que nous avons réalisé pour rendre à nos jantes un bel aspect.
+    </p>
 
-    <!-- DEUX COLONNES -->
+    <br/>
+    <br/>
+    <h3>Étape préliminaire</h3>
+    <br/>
 
-	<div class="container">
+    <p class="paragraphe">
+        Lors de l'achat de nos jantes, nous avons bien vérifié que celles-ci n'étaient
+        <span class="tertiary-color">pas fissurées</span>. Dans notre cas, les quatre jantes ne comportaient que
+        des défauts d'ordre esthétique, donc facilement réparables.
+	</p>
+
+    <p class="paragraphe">
+        Il est important de garder à l'esprit que les jantes en aluminium n'utilisent pas les mêmes boulons que celles en tôle :
+        Les jantes de 205 GTI nécessitent des boulons à <span class="tertiary-color">assise plate</span>.
+	</p>
+    <div class="text-center">
+        <figure class="figure">
+            <img src="/images/wheels_repair/1.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante">
+            <figcaption class="figure-caption text-right text-light">État de la pire jante avant travaux</figcaption>
+        </figure>
+    </div>
+
+    <br/>
+    <br/>
+    <h3>Étape 1 : ponçage</h3>
+    <br/>
+
+    <p class="paragraphe">
+        La première étape que nous avons réalisé est le ponçage de l'ensemble de chaque jante. Ce ponçage a permi de retirer
+        l'ancienne peinture qui s'écaillait par endroit, et de lisser la jante sur toute sa surface.
+	</p>
+
+    <p class="paragraphe">
+        Si la jante comporte un impact qui a retiré un bon morceau d'aluminium (facilement visible à l'œil nu), il peut être
+        intéressant de rajouter de la matière pour égaliser la surface. Dans ce cas, un <span class="tertiary-color">mastic aluminium</span> peut vous servir.
+	</p>
+
+    <p class="paragraphe">
+        Pour le ponçage, nous avons procédé en respectant les grains suivants:
+	</p>
+    <ol class="list">
+        <li>Grain 180</li>
+        <li>Grain 300</li>
+        <li>Grain 450</li>
+    </ol>
+
+    <p class="paragraphe">
+        Nous constatons qu'à la suite du ponçage (fastidieux), les jantes perdent leur brillant mais sont bien plus lisses au toucher.
+	</p>
+
+    <div class="text-center">
+        <figure class="figure">
+            <img src="/images/wheels_repair/2.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante après ponçage">
+            <figcaption class="figure-caption text-right text-light">État de la pire jante après ponçage</figcaption>
+        </figure>
+    </div>
+
+    <br/>
+    <br/>
+    <h3>Étape 2 : dégraissage</h3>
+    <br/>
+
+    <p class="paragraphe">
+        Une fois les quatre jantes bien poncées, nous les avons correctement nettoyées pour enlever les impuretés (caoutchouc autour de la valve, poussière),
+        et nous les avons dégraissées.
+	</p>
+
+    <p class="paragraphe">
+        Pour notre dégraissage maison, nous avons simplement utilisé de l'eau avec du produit vaisselle ainsi qu'une éponge.
+	</p>
+
+    <br/>
+    <br/>
+    <h3>Étape 3 : préparation à la peinture</h3>
+    <br/>
+
+    <p class="paragraphe">
+        Nous avons décidé de faire monter des pneus neufs avant de passer à l'étape de la peinture, pour éviter les potentiels accrocs
+        qui peuvent survenir lors du montage. Une fois le montage et l'équilibrage des pneus réalisés, nous avons de nouveau nettoyé les
+        roues de toutes les traces survenues entre temps, et nous avons masqué les pneus par du journal et du ruban adhésif de masquage.
+	</p>
+    <p class="paragraphe">
+        <span class="badge badge-accent">Attention</span> Ne pas oublier de couvrir également les valves !
+	</p>
+
+    <div class="text-center">
+        <figure class="figure">
+            <img src="/images/wheels_repair/3.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante après ponçage">
+            <figcaption class="figure-caption text-right text-light">Les quatre roues poncées et masquées</figcaption>
+        </figure>
+    </div>
+
+    <p class="paragraphe">
+        Important à noter: Les jantes de GTI 1.6L originales possèdent un centre dit "usiné". Le centre est donc poncé de manière circulaire jusqu'à un grain
+        d'environ 400. Si vous voulez faire cette manipulation, n'oubliez pas de masquer également le centre avec du ruban adhésif. Dans notre cas,
+        nous avons fait le choix de peindre le centre.
+	</p>
+
+    <br/>
+    <br/>
+    <h3>Étape 4 : apprêt</h3>
+    <br/>
+
+    <p class="paragraphe">
+        Maintenant que nos roues sont prêtes à être peintes, nous pouvons passer à l'application de deux couches d'apprêts (ou <i>primer</i>).
+        Cette étape est très importante car l'apprêt permet à la peinture de mieux tenir à la jante.
+	</p>
+
+    <p class="paragraphe">
+        Nous avons utilisé des bombes d'apprêt spécial pour surfaces en métal, et après quelques recherches, de nombreuses personnes l'ont déjà utilisé pour
+        leurs jantes aluminium.
+	</p>
+
+    <p class="paragraphe">
+        Nous avons donc appliqué une première couche d'apprêt, que nous avons laissé sécher avant de poncer au grain 400 les petites traces restantes (projections de peinture, ...).
+	</p>
+
+    <p class="paragraphe">
+        <span class="badge badge-quartiary">Conseil</span> Pour que la peinture par bombe se passe bien, veillez à suivre les préconisations du vendeur.
+        Dans notre cas, nous avons attendu d'avoir une température ambiante d'au moins 12°C, et nous avons laissé sécher au moins 24h entre les différentes couches.
+	</p>
+
+    <p class="paragraphe">
+        <span class="badge badge-quartiary">Conseil 2</span> Pensez à utiliser la bombe par petits sprays, dans plusieurs sens (vertical puis horizontal par exemple), afin de couvrir correctement toute la surface.
+	</p>
+
+    <div class="container">
 		<div class="row">
 			<div class="col-sm">
 				<br/>
-				<p class="paragraphe">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia aliquid eum, accusamus laudantium tenetur fugit reiciendis rem vitae soluta odio porro vero illo. Voluptatem amet quia, cum iure iusto autem.
-				</p>
+				<div class="text-center">
+                    <figure class="figure">
+                        <img src="/images/wheels_repair/4.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante après ponçage">
+                        <figcaption class="figure-caption text-right text-light">Les quatre roues après la première couche d'apprêt</figcaption>
+                    </figure>
+                </div>
 			</div>
 			<div class="col-sm">
 				<br/>
-				<p class="paragraphe">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid consectetur molestiae ducimus mollitia quo minus expedita tempora, aspernatur suscipit eos inventore. Hic optio, quaerat commodi laborum eligendi facilis minima accusantium!
-				</p>
-		  	</div>
-		</div>
-	  </div>
-
-      <!-- TROIS COLONNES -->
-
-	<div class="container">
-		<div class="row">
-			<div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia aliquid eum, accusamus laudantium tenetur fugit reiciendis rem vitae soluta odio porro vero illo. Voluptatem amet quia, cum iure iusto autem.
-				</p>
-			</div>
-			<div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid consectetur molestiae ducimus mollitia quo minus expedita tempora, aspernatur suscipit eos inventore. Hic optio, quaerat commodi laborum eligendi facilis minima accusantium!
-				</p>
+				<div class="text-center">
+                    <figure class="figure">
+                        <img src="/images/wheels_repair/4bis.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante après ponçage">
+                        <figcaption class="figure-caption text-right text-light">Détail de l'apprêt sur la pire jante</figcaption>
+                    </figure>
+                </div>
 		  	</div>
               <div class="col-sm">
 				<br/>
-				<p class="paragraphe">
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat aspernatur sit atque omnis cupiditate qui ipsam assumenda quos. Amet similique qui sint neque distinctio necessitatibus voluptatem minus, debitis veniam temporibus.
-				</p>
+				<div class="text-center">
+                    <figure class="figure">
+                        <img src="/images/wheels_repair/4ter.jpg" class="figure-img img-fluid rounded" style="width: 80%;" alt="Pire jante après ponçage">
+                        <figcaption class="figure-caption text-right text-light">Il est nécessaire de poncer !</figcaption>
+                    </figure>
+                </div>
 			</div>
 		</div>
     </div>
+    
 
-    <!-- TITRES -->
+    <br/>
+    <br/>
+    <h3>Étape 5 : peinture</h3>
+    <br/>
 
-    <h2>Sous titre</h2>
+    <br/>
+    <br/>
+    <h3>Étape 6 : vernis</h3>
+    <br/>
 
-    <h3>Sous sous titre</h3>
+    <br/>
+    <br/>
+    <h3>Résultat</h3>
+    <br/>
 
-    <!-- LISTES -->
+    <br/>
+    <br/>
+    <h3>Source</h3>
+    <br/>
 
-    <ul class="list">
-        <li>Element 1</li>
-        <li>Element 2</li>
-        <li>Element 3</li>
-    </ul>
-
-    <ol class="list">
-        <li>Element 1</li>
-        <li>Element 2</li>
-        <li>Element 3</li>
-    </ol>
-
-    <!-- IMAGE AVEC LEGENDE -->
-
-    <figure class="figure">
-        <img src="/images/205-neige.jpg" class="figure-img img-fluid rounded" alt="Desc">
-        <figcaption class="figure-caption text-right text-light">Légende</figcaption>
-    </figure>
-
-    <!-- JUMBOTRON -->
-
-    <div class="jumbotron">
-    <h1 class="display-4">Titre joli</h1>
-    <p class="lead">Texte important, citation, etc</p>
-
-    <!-- SEPARATOR -->
-    <hr class="my-4">
-    <p>Autre texte</p>
-    </div>
-
-    <!-- BADGES -->
-
-    <span class="badge badge-accent">Accent</span>
-    <span class="badge badge-primary">Primary</span>
-    <span class="badge badge-secondary">Secondary</span>
-    <span class="badge badge-tertiary">Tertiary</span>
-    <span class="badge badge-quartiary">Quartiary</span>
-
-    <!-- TABLEAU -->
-
-    <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-    </table>
+    <p class="paragraphe">
+        La principale source que nous avons utilisé est un message de l'utilisateur <i>3dfx</i> le forum <i>planete-205</i> : <a target="_blank" href="https://www.planete-205.com/forum/refection-jantes-gti-1600-t43576.html">Réfection jantes GTI 1600</a>
+	</p>
 
 	
 </div>
@@ -180,7 +244,7 @@
 
     h3 {
         font-size: large;
-        text-align: center;
+        text-align: left;
         color: var(--accent-color);
     }
 
@@ -208,4 +272,7 @@
         background-color: var(--quartiary-color);
     }
     
+    .figure-caption {
+        font-style: italic;
+    }
 </style>
