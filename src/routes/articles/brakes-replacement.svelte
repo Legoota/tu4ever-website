49bbea824a571da17c1c0343277d36bdc1de36a2
Léo B <leodu39200@gmail.com>
2022-04-23 11:32:17 +0200

    Adding article part on rear brakes

--- src/routes/articles/brakes-replacement.svelte
+++ src/routes/articles/brakes-replacement.svelte
@@ -19,12 +19,14 @@
     <br/>
     <h3>Étape 1 : Ouverture</h3>
     <br/>
+
+
     <p class="paragraphe">
         Après avoir posé la voiture sur chandelles, ôté les roues arrières et désseré le frein à main, nous avons retiré le cache du moyeu. 
         Nous avons enuite pu défreiner l'écrou à l'aide d'un marteau et d'un burin.
     </p>
     <p class="paragraphe">
-        Nous avons ensuite, à l'aide du marteau et du burin d'un côté et d'une cale de bois de l'autre, retiré le tambour. Nous avons longuement martellé la pièce et pulsirisé du dégripant pour la désolidariser de l'ensemble.
+        Nous avons ensuite, à l'aide du marteau et du burin d'un côté et d'une cale de bois de l'autre, retiré le tambour. Nous avons longuement martellé la pièce et pulverisé du dégripant pour la désolidariser de l'ensemble.
     </p>
     <br/>
     <div class="container">
@@ -44,135 +46,73 @@
 		  	</div>
 		</div>
 	  </div>
-    <!-- IMAGE -->
-
-	<img src="/images/205-neige.jpg" class="img-fluid rounded mx-auto d-block" alt="Desc">
-
-    <!-- DEUX COLONNES -->
-
-	<div class="container">
+    <br/>
+    <h3>Étape 2 : Dépose de l'ancien kit</h3>
+    <br/>
+    <p class="paragraphe">
+        Une fois le tambour déposé, nous avons accès à la machoire et aux segments garnis. Avant de démonter l'ancien kit de frein, nous avons vérifié que le nouveau était identique.
+        Nous avons enlevé les deux ressorts de maintient et retiré le câble de frein a main à l'aide de deux pinces. 
+    </p>
+    <p class="paragraphe">
+        A cette étape, il faudrait normalement remplacer le cylindre de roue, chose que nous avons décidé de ne pas faire car l'arrivée du circuit de liquide de frein est tellement soudé par la rouille que nous ne voulions pas casser la pièce. Nous remplacerons cette pièce dans le futur lorsque nous changerons l'intégralité des tuyaux du circuit de frein.
+        Nous avons également protégé les roulements pour appliquer un peu de convertisseur de rouille sur la flasque afin de lui redonner un état plus rassurant.
+    </p>
+    <br/>
+    <div class="container">
 		<div class="row">
 			<div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia aliquid eum, accusamus laudantium tenetur fugit reiciendis rem vitae soluta odio porro vero illo. Voluptatem amet quia, cum iure iusto autem.
-				</p>
+				<figure class="figure">
+                    <img src="/images/brakes-replacement/3.jpg" class="figure-img img-fluid rounded" alt="Old brake kit">
+                    <figcaption class="figure-caption text-right text-light">Aperçu de l'ancien kit de frein</figcaption>
+                </figure>
 			</div>
+            <div class="col-1"></div>
 			<div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid consectetur molestiae ducimus mollitia quo minus expedita tempora, aspernatur suscipit eos inventore. Hic optio, quaerat commodi laborum eligendi facilis minima accusantium!
-				</p>
+                <figure class="figure">
+                    <img src="/images/brakes-replacement/4.jpg" class="figure-img img-fluid rounded" alt="Brake fluid arrival">
+                    <figcaption class="figure-caption text-right text-light">Raccordement rouillé du circuit de freinage</figcaption>
+                </figure>
 		  	</div>
 		</div>
 	  </div>
-
-      <!-- TROIS COLONNES -->
-
-	<div class="container">
+    <br/>
+    <br/>
+    <h3>Étape 3 : Nettoyage, graissage et remontage</h3>
+    <br/>
+    <div class="container">
 		<div class="row">
 			<div class="col-sm">
-				<br/>
 				<p class="paragraphe">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia aliquid eum, accusamus laudantium tenetur fugit reiciendis rem vitae soluta odio porro vero illo. Voluptatem amet quia, cum iure iusto autem.
-				</p>
+                    Nous avons ensuite bien nettoyé avec du nettoyant frein la flasque, la bande roulement du tambour, et le roulement à rouleaux coniques. Puis nous avons séché le tout avec un chiffon propre. Nous avons ensuite appliqué la graisse haute température fournie dans le kit sur les emplacement où repose les nouvelles machoires de frein et de la graisse solide adaptée sur les roulements.
+                </p>
+                <p class="paragraphe">
+                    Nous sommes ensuite passé au remontage, d'abord nous avons reconnecté le câble de frein à main, puis remis en place les machoires, bien alignées sur le cylindre et la butée. Nous avons ensuite remis les ressorts de maintien et enfin le tambour. Le tambours doit frotter très légèrement et uniformément quand on le tourne. Sur certaines voiture, nous pouvons agir sur le ressort de rattrapage automatique de jeu de frein. Ce n'est pas notre cas sur notre Peueot 205.
+                </p>
 			</div>
+            <div class="col-1"></div>
 			<div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid consectetur molestiae ducimus mollitia quo minus expedita tempora, aspernatur suscipit eos inventore. Hic optio, quaerat commodi laborum eligendi facilis minima accusantium!
-				</p>
+				<figure class="figure">
+                    <img src="/images/brakes-replacement/5.jpg" class="figure-img img-fluid rounded" alt="Lubrication">
+                    <figcaption class="figure-caption text-right text-light">Graissage de la flasque et roulements avant remontage</figcaption>
+                </figure>
 		  	</div>
-              <div class="col-sm">
-				<br/>
-				<p class="paragraphe">
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat aspernatur sit atque omnis cupiditate qui ipsam assumenda quos. Amet similique qui sint neque distinctio necessitatibus voluptatem minus, debitis veniam temporibus.
-				</p>
-			</div>
 		</div>
-    </div>
-
-    <!-- TITRES -->
-
-    <h2>Sous titre</h2>
-
-    <h3>Sous sous titre</h3>
-
-    <!-- LISTES -->
-
-    <ul class="list">
-        <li>Element 1</li>
-        <li>Element 2</li>
-        <li>Element 3</li>
-    </ul>
-
-    <ol class="list">
-        <li>Element 1</li>
-        <li>Element 2</li>
-        <li>Element 3</li>
-    </ol>
-
-    <!-- IMAGE AVEC LEGENDE -->
-
+	</div>
+    <br/>
+    <p class="paragraphe">
+        Voici le résultat final avant remise en place du tambour.
+    </p>
     <figure class="figure">
-        <img src="/images/205-neige.jpg" class="figure-img img-fluid rounded" alt="Desc">
-        <figcaption class="figure-caption text-right text-light">Légende</figcaption>
+        <img src="/images/brakes-replacement/6.jpg" class="figure-img img-fluid rounded" alt="New brake kit">
+        <figcaption class="figure-caption text-right text-light">Nouveau kit de frein</figcaption>
     </figure>
 
-    <!-- JUMBOTRON -->
+    <br/>
+    <br/>
+    <h2>Les disques et plaquettes avant</h2>
 
-    <div class="jumbotron">
-    <h1 class="display-4">Titre joli</h1>
-    <p class="lead">Texte important, citation, etc</p>
-
-    <!-- SEPARATOR -->
-    <hr class="my-4">
-    <p>Autre texte</p>
-    </div>
-
-    <!-- BADGES -->
-
-    <span class="badge badge-accent">Accent</span>
-    <span class="badge badge-primary">Primary</span>
-    <span class="badge badge-secondary">Secondary</span>
-    <span class="badge badge-tertiary">Tertiary</span>
-    <span class="badge badge-quartiary">Quartiary</span>
-
-    <!-- TABLEAU -->
-
-    <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-    </table>
-
-	
 </div>
+
 
 <style>
 	.content {
