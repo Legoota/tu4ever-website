04904b152983a03f3c9d68434cc89d25aba6d03f
Léo Krebs <leokrebs68@gmail.com>
2022-04-24 15:08:27 +0200

    Adding base path for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: null,
+			paths: {
+				base: dev ? '' : '/tu4ever-website',
+			}
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
